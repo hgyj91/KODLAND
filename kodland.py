539943d31e48158b1e0e398bcb13b25258892a32
hgyj91 <seba.roz01@gmail.com>
2024-09-07 13:30:28 +0200

    Create kodland.py

---
+++ kodland.py
@@ -0,0 +1,3 @@
+import random
+list = ["Chodzę do 7 klasy","Uwielbiam grać w piłkę","Mam urodziny w listopadzie","Mieszkam w Krakowie","Uwielbiam FC Barcelonę oraz Wisłę Kraków!"]
+print(random.choice(list))
