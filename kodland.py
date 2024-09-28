9ddd9c3e5f1ab1865175216d4f7f681aa4ce8960
hgyj91 <seba.roz01@gmail.com>
2024-09-28 13:01:23 +0200

    Update kodland.py

--- kodland.py
+++ kodland.py
@@ -1,3 +1,21 @@
-import random
-list = ["Chodzę do 7 klasy","Uwielbiam grać w piłkę","Mam urodziny w listopadzie","Mieszkam w Krakowie","Uwielbiam FC Barcelonę oraz Wisłę Kraków!"]
-print(random.choice(list))
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
